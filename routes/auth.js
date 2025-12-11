7dde8bf82c331f3762dc7318ca874fe9c7924d3a
wicopt <mashaushakova55@gmail.com>
2025-12-11 14:35:22 +0700

    fgh

--- routes/auth.js
+++ routes/auth.js
@@ -175,8 +175,9 @@ router.post("/login", (req, res, next) => {
 
       console.log("✅ Login successful, user ID:", user.user_id);
       
-      id = user.user_id
-      formatUser = User.findById(id)
+      id = user.user_id;
+      formatUser = User.findById(id);
+      console.log(formatUser);
       return res.json( formatUser );
     });
   })(req, res, next);
